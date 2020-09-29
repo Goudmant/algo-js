93db7f1cd98f83021a4d0a158716e4b09a58d735
Goudmant <yvangoudmant@gmail.com>
2020-09-29 16:32:43 +0200

    save

---
+++ js/3/3.1.js
@@ -0,0 +1,14 @@
+const readlineSync = require("readline-sync");
+
+let nums1 = [1, 2, 3, 4, 5];
+let nums2 = [100, 101, 102];
+let total1 = 0;
+let total2 = 0;
+
+for (let elem of nums1) {
+    total1 += new Number(elem);
+}
+for (let elem of nums2) {
+    total2 += new Number(elem);
+}
+console.log(total1 + " and " + total2);
---
+++ js/3/3.2.js
@@ -0,0 +1,11 @@
+const readlineSync = require("readline-sync");
+
+let nums1 = [1, 2, 3, 4, 5];
+let nums2 = [100, 101, 102];
+let total1 = 0;
+let total2 = 0;
+
+for (let elem of nums1) {
+    Number += new Number(elem 2);
+}
+AAAAAAAAAAAAAAAAAAAAH
