51f355d2463c2c1b4ce19d18340d5ad01ea0b18d
Goudmant <yvangoudmant@gmail.com>
2020-09-30 17:00:12 +0200

    save

--- js/4/4.4.js
+++ js/4/4.4.js
@@ -1,4 +1,10 @@
 const readlineSync = require("readline-sync");
 
-
-
+function average(arr) {
+    let nums = [1, 2, 3, 4, 5];
+    let total = 0;
+    for (let elem of nums) {
+        total += new Number(elem);
+    }
+}
+console.log(total / nums.length);
