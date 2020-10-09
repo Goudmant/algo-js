0112db3d859bbe1d1195403a2b72b1faac444500
Goudmant <yvangoudmant@gmail.com>
2020-10-09 09:24:15 +0200

    save

--- 05-arrays/08-array-includes/script.js
+++ 05-arrays/08-array-includes/script.js
@@ -1,22 +1,28 @@
 // 05-arrays/08-array-includes/script.js - 5.8: presence in an aray
 
 (() => {
+  const fruits = [
+    "apple",
+    "perry",
+    "strawberry",
+    "tomato",
+    "kiwi",
+    "banana",
+    "orange",
+    "mandarin",
+    "durian",
+    "peach",
+    "grapes",
+    "cherry",
+  ];
 
-    const fruits = [
-        "apple",
-        "perry",
-        "strawberry",
-        "tomato",
-        "kiwi",
-        "banana",
-        "orange",
-        "mandarin",
-        "durian",
-        "peach",
-        "grapes",
-        "cherry",
-    ];
+  const run = document.getElementById("run");
 
-    // your code here
-
+  run.addEventListener("click", () => {
+    if (fruits.includes("apple")) {
+      console.log("oui");
+    } else {
+      console.log("non");
+    }
+  });
 })();
