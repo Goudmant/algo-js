78912f02f583a45f4c4410af84fa8ae415666b73
Goudmant <yvangoudmant@gmail.com>
2020-10-11 15:36:30 +0200

    save

--- 07-classes/02-methods/script.js
+++ 07-classes/02-methods/script.js
@@ -1,6 +1,21 @@
 //07-classes/02-methods/script.js - 7.2: methods
 
-
 (() => {
-    // your code here
+  class Person {
+    constructor(firstname, lastname) {
+      this.firstname = firstname;
+      this.lastname = lastname;
+    }
+    sayHello() {
+      return `hello, ${this.firstname} ${this.lastname}`;
+    }
+  }
+  const run = document.getElementById("run");
+
+  run.addEventListener("click", () => {
+    let Yvan = new Person("Yvan", "Goudmant");
+    console.log(Yvan.sayHello());
+    alert`${sayHello}`;
+  });
 })();
+// alert ne fonctionne pas du tout
